f053dd728e13bbdf8e556d1970e36e8afaa7b2ab
Jaseem Abid <jaseemabid@gmail.com>
2017-05-27 13:54:26 -0400

    Switch to slightly more complex template, disable optimizations

--- lllvm/hello.c
+++ lllvm/hello.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
+int fib(int n) {
+    if (n == 0) {
+        return 1;
+    } else {
+        return n * fib(n - 1);
+    }
+}
+
 int main() {
-  printf("hello world\n");
-  return 34;
+    printf("hello %d\n", fib(5));
+    return 0;
 }
